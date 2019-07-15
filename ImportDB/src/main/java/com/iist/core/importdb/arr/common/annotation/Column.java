59d9af908fb7667657fdc8e09aa093545fd10b6e
unknown <vanhungnguyen.vinhuni@gmail.com>
2019-07-15 17:45:24 +0700

    3st commit

---
+++ ImportDB/src/main/java/com/iist/core/importdb/arr/common/annotation/Column.java
@@ -0,0 +1,16 @@
+package com.iist.core.importdb.arr.common.annotation;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Target(ElementType.FIELD)
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Column {
+	String BLANK = "";
+	String name() default BLANK;
+}
---
+++ ImportDB/src/main/java/com/iist/core/importdb/arr/common/annotation/Model.java
@@ -0,0 +1,7 @@
+package com.iist.core.importdb.arr.common.annotation;
+
+public abstract class Model {
+	@Header( indexHeader =0)
+	public int indexHeader =0;
+
+}
